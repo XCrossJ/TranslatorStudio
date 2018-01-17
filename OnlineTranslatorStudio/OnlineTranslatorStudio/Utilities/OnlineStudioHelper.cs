e9e238d2e749b62b2372073019672fdbd960ec4e
XCrossJ <xcrossj.mayonakasignal@gmail.com>
2018-01-17 22:35:38 +1100

    Constructed proof of concept javascript translation desk

---
+++ OnlineTranslatorStudio/OnlineTranslatorStudio/Utilities/OnlineStudioHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTranslatorStudio.Utilities
+{
+    public static class OnlineStudioHelper
+    {
+        public static IHtmlString ToJson(this HtmlHelper helper, object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            settings.Converters.Add(new JavaScriptDateTimeConverter());
+            return helper.Raw(JsonConvert.SerializeObject(obj, settings));
+        }
+
+        public static string ToJson(object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            settings.Converters.Add(new JavaScriptDateTimeConverter());
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
