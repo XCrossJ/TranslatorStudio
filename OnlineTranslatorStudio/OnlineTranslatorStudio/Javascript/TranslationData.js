170a6d05609ba21f2286b226793ab3985aced5f1
XCrossJ <xcrossj.mayonakasignal@gmail.com>
2018-01-19 17:48:05 +1100

    Fleshed out Translation Data JS class. Require further testing for JS Handler

---
+++ OnlineTranslatorStudio/OnlineTranslatorStudio/Javascript/TranslationData.js
@@ -0,0 +1,92 @@
+﻿//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+class ProjectData {
+    constructor(name, raw, translated, marked, completed) {
+        this.projectName = name;
+        this.rawLines = raw;
+        this.translatedLines = translated;
+        this.markedLines = marked;
+        this.completedLines = completed;
+    }
+}
+
+class TranslationData {
+
+    // Constructor
+    constructor(data) {
+        this.projectData = data;
+        
+        this.currentIndex = 0;
+        this.maxIndex = data.rawLines.length - 1;
+
+        this.defaultTranslationMode = true;
+    }
+
+    // Get Properties
+    get ProjectName() {
+        return this.projectData.projectName;
+    }
+
+    get RawLines() {
+        return this.projectData.rawLines;
+    }
+
+    get TranslatedLines() {
+        return this.projectData.translatedLines;
+    }
+
+    get MarkedLines() {
+        return this.projectData.markedLines;
+    }
+
+    get CompletedLines() {
+        return this.projectData.completedLines;
+    }
+
+    get CurrentRaw() {
+        return this.RawLines[this.currentIndex];
+    }
+
+    get CurrentTranslation() {
+        return this.TranslatedLines[this.currentIndex];
+    }
+
+    get CurrentMarked() {
+        return this.MarkedLines[this.currentIndex];
+    }
+
+    get CurrentCompletion() {
+        return this.CompletedLines[this.currentIndex];
+    }
+
+    get NumberOfLines() {
+        return this.RawLines.length;
+    }
+
+    get NumberOfCompletedLines() {
+        return DetermineCompletedLines(this.CompletedLines);
+    }
+
+    // Methods
+    IncrementIndex() {
+        if (translationData.currentIndex < translationData.maxIndex) {
+            translationData.currentIndex++;
+        }
+    }
+
+    DecrementIndex() {
+        if (translationData.currentIndex > 0) {
+            translationData.currentIndex--;
+        }
+    }
+
+    DetermineCompletedLines(completedLines) {
+        var result = 0;
+        for (var i = 0; i < completedLines.length; i++) {
+            if (completedLines[i] == true) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ OnlineTranslatorStudio/OnlineTranslatorStudio/Javascript/TranslationDeskHandler.js
@@ -0,0 +1,28 @@
+﻿//https://jsfiddle.net/5o1wf1bn/1/
+
+var rawSelector = "#CurrentRaw";
+var translationSelector = "#CurrentTranslation";
+var markedSelector = "#CurrentMarked";
+var completionSelector = "#CurrentCompletion";
+
+function incrementIndex(data) {
+    data.IncrementIndex();
+    reloadDesk(data);
+}
+
+function decrementIndex(data) {
+    data.DecrementIndex();
+    reloadDesk(data);
+}
+
+//function changeMarked(data, marked) {
+//    data.Marked = marked;
+//    reloadDesk();
+//}
+
+function reloadDesk(data) {
+    $(rawSelector).text(data.CurrentRaw);
+    $(translationSelector).text(data.CurrentTranslation);
+    $(markedSelector).prop('checked', translationData.CurrentMarked);
+    $(completionSelector).prop('checked', translationData.CurrentCompletion);
+}
