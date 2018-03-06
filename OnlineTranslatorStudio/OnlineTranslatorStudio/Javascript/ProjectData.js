2e2b3e35243b90012f47c31f6bdcefd11f881a1a
XCrossJ <xcrossj.mayonakasignal@gmail.com>
2018-03-06 18:47:38 +1100

    Moved javascript to typescript

---
+++ OnlineTranslatorStudio/OnlineTranslatorStudio/Javascript/ProjectData.js
@@ -0,0 +1,11 @@
+var ProjectData = /** @class */ (function () {
+    function ProjectData(name, raw, translated, marked, completed) {
+        this.projectName = name;
+        this.rawLines = raw;
+        this.translatedLines = translated;
+        this.markedLines = marked;
+        this.completedLines = completed;
+    }
+    return ProjectData;
+}());
+//# sourceMappingURL=ProjectData.js.map
